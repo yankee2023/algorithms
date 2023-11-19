9782d2c0f5c7268802114af004763d1c107e52ae
yankee <tad.training2023@gmail.com>
2023-11-19 21:46:47 +0900

    ALDS1_2_D: Shell Sort

---
+++ ALDS1_2_D_Shell-Sort/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+long long cnt;
+int A[10000000];
+int n;
+vector<int> G;
+
+/**
+ * @brief 間隔gを指定した挿入ソート
+*/
+static void insertionSort(int A[], int n, int g) {
+    for (int i = g; i < n; i++) {
+        int v = A[i];
+        int j = i - g;
+        while (j >= 0 && A[j] > v) {
+            A[j+g] = A[j];
+            j -= g;
+            cnt++;
+        }
+        A[j+g] = v;
+    }
+}
+
+/**
+ * @brief シェルソート
+ * 数列G = {1, 4, 13, 40, 121, 364, 1093, ...}を生成
+*/
+static void shellSort(int A[], int n) {
+    for (int h = 1; ; ) {
+        if (h > n) {
+            break;
+        }
+        G.push_back(h);
+        h = 3*h + 1;
+    }
+
+    for (int i = G.size() - 1; i >= 0; i--) {
+        // 逆順にG[i] = g を指定
+        insertionSort(A, n, G[i]);
+    }
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    cnt = 0;
+
+    shellSort(A, n);
+
+    cout << G.size() << endl;
+    for (int i = G.size() - 1; i >= 0; i--) {
+        cout << G[i];
+        if (i) {
+            cout << " ";
+        }
+    }
+    cout << endl;
+    cout << cnt << endl;
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", A[i]);
+    }
+
+    return 0;    
+}
